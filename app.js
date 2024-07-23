27331b20ff1aa533d2d8868e635f29f26c92542e
dylanstead <dylanstead@gmail.com>
2024-07-23 10:19:05 +0100

    added cors

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const fruitRouter = require('./routes/fruitRouter')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const fruitRouter = require("./routes/fruitRouter");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+// CORS - Cross origin resource sharing
+app.use(cors());
+app.use(express.json());
+app.use("/fruits", fruitRouter);
 
-app.use(express.json())
-app.use('/fruits', fruitRouter)
-
-
-module.exports = app
+module.exports = app;
